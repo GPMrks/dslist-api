dce47f84a354172fa55607281116965c1eb308f2
Guilherme Marques <guilhermepereiramarques@hotmail.com>
2023-06-02 16:39:17 -0300

    DOCS - Completed Swagger Documentation

--- src/main/java/com/gpmrks/dslistapi/Controllers/GameController.java
+++ src/main/java/com/gpmrks/dslistapi/Controllers/GameController.java
@@ -4,6 +4,8 @@ import com.gpmrks.dslistapi.Dto.GameDTO;
 import com.gpmrks.dslistapi.Dto.MinimalGameInfoDTO;
 import com.gpmrks.dslistapi.Services.BelongingService;
 import com.gpmrks.dslistapi.Services.GameService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/games")
+@Tag(name = "Games Controller")
 public class GameController {
 
     private GameService gameService;
@@ -26,18 +29,21 @@ public class GameController {
     }
 
     @GetMapping
+    @Operation(summary = "Get All the Games", description = "Returns a list of all Games")
     public ResponseEntity<List<MinimalGameInfoDTO>> getAllGames() {
         List<MinimalGameInfoDTO> gameDTOList = gameService.getAllGames();
         return ResponseEntity.ok(gameDTOList);
     }
 
     @GetMapping("{gameId}")
+    @Operation(summary = "Get a specific Game", description = "Returns a specific Game by its ID")
     public ResponseEntity<GameDTO> getGameById(@PathVariable Long gameId) {
         GameDTO gameDTO = gameService.getGameById(gameId);
         return ResponseEntity.ok(gameDTO);
     }
 
     @PostMapping
+    @Operation(summary = "Create a Game", description = "Creates a Game")
     public ResponseEntity<GameDTO> createGame(@RequestBody GameDTO gameDTO, UriComponentsBuilder uriComponentsBuilder) {
         GameDTO game = gameService.createGame(gameDTO);
         URI uri = uriComponentsBuilder.path("/games/{gameId}").buildAndExpand(game.game().getId()).toUri();
@@ -45,18 +51,21 @@ public class GameController {
     }
 
     @PutMapping("{gameId}")
+    @Operation(summary = "Update the whole Game", description = "Updates the Game resource as a whole by its Game ID")
     public ResponseEntity<GameDTO> updateGame(@PathVariable Long gameId, @RequestBody GameDTO gameDTO) {
         GameDTO gameUpdated = gameService.updateGame(gameId, gameDTO);
         return ResponseEntity.ok(gameUpdated);
     }
 
     @PatchMapping("{gameId}")
+    @Operation(summary = "Update a Game Attribute", description = "Updates a desired field of the Game by its ID")
     public ResponseEntity<GameDTO> updatePartiallyGame(@PathVariable Long gameId, @RequestBody GameDTO gameDTO) {
         GameDTO gameUpdatedPartially = gameService.updatePartiallyGame(gameId, gameDTO);
         return ResponseEntity.ok(gameUpdatedPartially);
     }
 
     @DeleteMapping("{gameId}")
+    @Operation(summary = "Delete a Game", description = "Deletes a specific Game by its ID")
     public ResponseEntity<Void> deleteGame(@PathVariable Long gameId) {
         gameService.deleteGame(gameId);
         return ResponseEntity.noContent().build();
--- src/main/java/com/gpmrks/dslistapi/Controllers/ListController.java
+++ src/main/java/com/gpmrks/dslistapi/Controllers/ListController.java
@@ -4,6 +4,8 @@ import com.gpmrks.dslistapi.Dto.*;
 import com.gpmrks.dslistapi.Services.BelongingService;
 import com.gpmrks.dslistapi.Services.GameService;
 import com.gpmrks.dslistapi.Services.ListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/lists")
+@Tag(name = "Lists Controller")
 public class ListController {
 
     private ListService listService;
@@ -30,18 +33,21 @@ public class ListController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all Lists of Games", description = "Returns a list of all Lists of Games")
     public ResponseEntity<List<ListDTO>> getAllGameLists() {
         List<ListDTO> listDTO = listService.getAllGameList();
         return ResponseEntity.ok(listDTO);
     }
 
     @GetMapping("{listId}")
+    @Operation(summary = "Get a specific List of Games", description = "Returns all the games of a specific list of games")
     public ResponseEntity<List<MinimalGameInfoDTO>> searchByList(@PathVariable Long listId) {
         List<MinimalGameInfoDTO> minimalGameInfoDTOS = listService.searchByList(listId);
         return ResponseEntity.ok(minimalGameInfoDTOS);
     }
 
     @PostMapping
+    @Operation(summary = "Create a List of Games", description = "Creates a new List of Games")
     public ResponseEntity<ListDTO> createList(@RequestBody @Valid ListForm listForm, UriComponentsBuilder uriComponentsBuilder) {
         ListDTO listSaved = listService.createList(listForm);
         URI uri = uriComponentsBuilder.path("lists/{listId}").buildAndExpand(listSaved.id()).toUri();
@@ -49,12 +55,14 @@ public class ListController {
     }
 
     @PostMapping("order-game")
+    @Operation(summary = "Order a Game in a List of Games", description = "Set a new position to a Game in the List by Game ID and the Position")
     public ResponseEntity<Void> orderGame(@RequestBody @Valid OrderForm orderForm) {
         belongingService.orderGameList(orderForm.listId(), orderForm.gameId(), orderForm.destinationIndex());
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("belongs")
+    @Operation(summary = "Register Game to List of Games", description = "Register a Game to a List of Games")
     public ResponseEntity<BelongingDTO> registerGameToList(@RequestBody @Valid BelongingForm belongingForm) {
         BelongingDTO belongingDTORegistered = belongingService.registerGameToList(belongingForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(belongingDTORegistered);
